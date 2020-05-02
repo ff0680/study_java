a39972ce78dbccb825afa082478b4a483ed2bd8c
hunter_lcy <513383660@qq.com>
2020-05-02 21:24:10 +0800

    init spring boot study

---
+++ spring-boot-demo/spring-boot-demo-web/src/main/java/cn/test/web/Application.java
@@ -0,0 +1,16 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2020 All Rights Reserved.
+ */
+package cn.test.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lcy
+ * @version Id: Application.java, v 0.1 2020/5/2 21:20  Exp $$
+ */
+@SpringBootApplication
+public class Application {
+
+}
